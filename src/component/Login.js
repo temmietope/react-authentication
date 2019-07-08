c9f4b55fd71a23b718027dfb924db23b2b0e004c
Temitope <temmieayodele@gmail.com>
2019-07-08 17:14:02 +0100

    editted UI

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,36 @@
 import React, {Component} from "react";
 
 class Login extends Component{
+    // async onSubmit(){
+    //     const response = await fetch("https://authuserapi.herokuapp.com/register", {
+    //         method: "post",
+    //         headers: {
+    //           Authorization: `${localStorage.setItem("authToken")}`,
+    //           "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //           name,
+    //           username,
+    //           password,
+    //           email,
+    //           address,
+              
+    //         })
+    //       }).then(res => res.json());
+    // }
+    
     render(){
         return(
-            <></>
+            <form onSubmit={(e) => {
+                this.onSubmit(e);
+              }}>
+                
+                <h4>Email</h4>
+                <input type="email"/>
+                <h4>Password</h4>
+                <input type="password"/>
+                
+            </form>
         )
     }
 }
