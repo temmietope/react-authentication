4a122b269a90bfb6729d883f50580a83e01342ea
Temitope <temmieayodele@gmail.com>
2019-07-08 17:13:29 +0100

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Register from "./Register"
+
+class Home extends Component{
+    register=(email, password)=>{
+        console.log(email, password)
+    }
+    render(){
+        return(
+            <>
+            <Register register={this.register}/>
+            </>
+        )
+    }
+}
+
+export default Home
