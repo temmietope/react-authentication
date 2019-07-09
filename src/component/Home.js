74f889378b3660e9f1be9fa92d876cb352035398
Temitope <temmieayodele@gmail.com>
2019-07-09 15:46:55 +0100

    added new props to the Register component

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,17 +1,17 @@
-import React, {Component} from "react";
-import Register from "./Register"
+import React, { Component } from "react";
+import Register from "./Register";
 
-class Home extends Component{
-    register=(email, password)=>{
-        console.log(email, password)
-    }
-    render(){
-        return(
-            <>
-            <Register register={this.register}/>
-            </>
-        )
-    }
+class Home extends Component {
+  register = (email, password) => {
+    console.log(email, password);
+  };
+  render() {
+    return (
+      <>
+        <Register register={this.register} {...this.props} />
+      </>
+    );
+  }
 }
 
-export default Home
+export default Home;
