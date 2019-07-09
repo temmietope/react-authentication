3ef94b10a152b412c153465c938c94626f46d3ed
Temitope <temmieayodele@gmail.com>
2019-07-09 15:50:56 +0100

    added a new route path

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./component/Home";
+
 import Register from "./component/Register";
 import Login from "./component/Login";
+import Profile from "./component/Profile";
+
 
 function App() {
   return (
     <div className="App">
       <Router>
         <>
-          <Route exact path="/" component={Register} />
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
+          <Route path="/profile" component={Profile} />
+
         </>
       </Router>
     </div>
