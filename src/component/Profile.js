7c858f0295e7f1c15b8c7c61cd0bbe65eba96dfa
Temitope <temmieayodele@gmail.com>
2019-07-09 15:49:23 +0100

    fetched for details

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,10 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Profile extends Component{
-    render(){
-        return(
-            <></>
-        )
-    }
+class Profile extends Component {
+  state = {
+    name: "",
+    username: "",
+    email: "",
+    address: ""
+  };
+  async componentDidMount() {
+    const user = await fetch("https://authuserapi.herokuapp.com/profile", {
+      method: "get",
+      headers: {
+        Authorization: `${localStorage.getItem("authToken")}`
+      }
+    }).then(res => res.json());
+    console.log(user);
+  }
+  render() {
+    return <>Name: UserName: Email: Address:</>;
+  }
 }
-export default Profile
+export default Profile;
