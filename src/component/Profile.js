5f6c83d125df19a3d90534c8d759162c829988f9
Temitope <temmieayodele@gmail.com>
2019-07-11 19:27:47 +0100

    imported link from react-router-dom and the UpdateProfile component

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,23 +1,47 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
 
 class Profile extends Component {
   state = {
-    name: "",
-    username: "",
-    email: "",
-    address: ""
+    user: ""
   };
   async componentDidMount() {
-    const user = await fetch("https://authuserapi.herokuapp.com/profile", {
+    await fetch("https://authuserapi.herokuapp.com/profile", {
       method: "get",
       headers: {
         Authorization: `${localStorage.getItem("authToken")}`
       }
-    }).then(res => res.json());
-    console.log(user);
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({ user: res });
+        console.log(this.state);
+      });
+  }
+  renderNewUser = () => {
+    const { user } = this.state;
+    return (
+      <main>
+        <section>
+          <p>Name: {user.name}</p>
+          <p>Username: {user.username}</p>
+          <p>Address: {user.address}</p>
+          <p>Email: {user.email}</p>
+        </section>
+        <Link to="/update">Update profile?</Link>
+      </main>
+    );
+  };
+  renderLoading() {
+    return <div>Loading...</div>;
   }
   render() {
-    return <>Name: UserName: Email: Address:</>;
+    return (
+      <div>
+        {this.state.user ? this.renderNewUser() : this.renderLoading()}
+      </div>
+    );
   }
 }
 export default Profile;
