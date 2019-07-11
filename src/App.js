011448e6a002502dd40a011fdbfabbd7e881966a
Temitope <temmieayodele@gmail.com>
2019-07-11 19:30:37 +0100

    imported UpdateProfile component and formatted code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./component/Home";
-
 import Register from "./component/Register";
 import Login from "./component/Login";
 import Profile from "./component/Profile";
-
+import UpdateProfile from "./component/UpdateProfile";
 
 function App() {
   return (
@@ -16,7 +15,7 @@ function App() {
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/profile" component={Profile} />
-
+          <Route path="/update" component={UpdateProfile} />
         </>
       </Router>
     </div>
